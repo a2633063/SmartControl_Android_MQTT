86831adda87d534fd5e592a28ca30240d99b3ff7
Zip <76966589@qq.com>
2019-02-28 14:29:24 +0800

    add:增加ButtonMateSettingFragment service bindService

--- app/src/main/java/com/zyc/zcontrol/controlItem/buttonmate/ButtonMateSettingFragment.java
+++ app/src/main/java/com/zyc/zcontrol/controlItem/buttonmate/ButtonMateSettingFragment.java
@@ -2,42 +2,89 @@ package com.zyc.zcontrol.controlItem.buttonmate;
 
 
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+
+import com.zyc.zcontrol.ConnectService;
 import com.zyc.zcontrol.R;
+
+import static android.content.Context.BIND_AUTO_CREATE;
+
 @SuppressLint("ValidFragment")
 public class ButtonMateSettingFragment extends PreferenceFragment {
     final static String Tag = "RGBSettingFragment_Tag";
     SharedPreferences mSharedPreferences;
     SharedPreferences.Editor editor;
-    String name=null;
-    String mac=null;
+
+    //region 使用本地广播与service通信
+    LocalBroadcastManager localBroadcastManager;
+    private MsgReceiver msgReceiver;
+    ConnectService mConnectService;
+    //endregion
 
 
-    public ButtonMateSettingFragment(String name,String mac) {
-        this.name=name;
-        this.mac=mac;
+    String name = null;
+    String mac = null;
+
+
+    public ButtonMateSettingFragment(String name, String mac) {
+        this.name = name;
+        this.mac = mac;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getPreferenceManager().setSharedPreferencesName("Setting_" + mac);
-        Log.d(Tag,"设置文件:"+"Setting" + mac);
+        Log.d(Tag, "设置文件:" + "Setting" + mac);
         addPreferencesFromResource(R.xml.button_mate_setting);
+
+
+        //region MQTT服务有关
+        //region 动态注册接收mqtt服务的广播接收器,
+        localBroadcastManager = LocalBroadcastManager.getInstance(getActivity().getApplicationContext());
+        msgReceiver = new MsgReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ConnectService.ACTION_MQTT_CONNECTED);
+        intentFilter.addAction(ConnectService.ACTION_MQTT_DISCONNECTED);
+        intentFilter.addAction(ConnectService.ACTION_DATA_AVAILABLE);
+        localBroadcastManager.registerReceiver(msgReceiver, intentFilter);
+        //endregion
+
+        //region 启动MQTT 服务以及启动,无需再启动
+        Intent intent = new Intent(getActivity().getApplicationContext(), ConnectService.class);
+        getActivity().bindService(intent, mMQTTServiceConnection, BIND_AUTO_CREATE);
+        //endregion
+        //endregion
+        
 //
 //        CheckBoxPreference mEtPreference = (CheckBoxPreference) findPreference("theme");
         EditTextPreference namePreference = (EditTextPreference) findPreference("name");
         EditTextPreference domoticz_idx = (EditTextPreference) findPreference("domoticz_idx");
 //
-        domoticz_idx.setOnPreferenceChangeListener(PreferenceChangeListener);
         domoticz_idx.setSummary(domoticz_idx.getText());
+        domoticz_idx.setOnPreferenceChangeListener(PreferenceChangeListener);
 
+        namePreference.setSummary(name);
+        namePreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
 
+                return false;
+            }
+        });
 
 
     }
@@ -50,6 +97,47 @@ public class ButtonMateSettingFragment extends PreferenceFragment {
         }
     };
 
+    //发送
 
+    //数据接收处理函数
+    void Receive(String topic, String message) {
+        //TODO 数据接收处理
+        Log.d(Tag, "RECV DATA,topic:" + topic + ",content:" + message);
+
+    }
+
+    //region MQTT服务有关
+
+    private final ServiceConnection mMQTTServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mConnectService = ((ConnectService.LocalBinder) service).getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mConnectService = null;
+        }
+    };
+
+    //广播接收,用于处理接收到的数据
+    public class MsgReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (ConnectService.ACTION_MQTT_CONNECTED.equals(action)) {  //连接成功
+                Log.d(Tag, "ACTION_MQTT_CONNECTED");
+            } else if (ConnectService.ACTION_MQTT_DISCONNECTED.equals(action)) {  //连接失败/断开
+                Log.w(Tag, "ACTION_MQTT_DISCONNECTED");
+            } else if (ConnectService.ACTION_DATA_AVAILABLE.equals(action)) {  //接收到数据
+                String topic = intent.getStringExtra(ConnectService.EXTRA_DATA_TOPIC);
+                String message = intent.getStringExtra(ConnectService.EXTRA_DATA_MESSAGE);
+                Receive(topic, message);
+            }
+        }
+    }
+    //endregion
 
 }
